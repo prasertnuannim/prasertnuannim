94c3ef248770c187b7e1231e89a5fe12f463b89c
sertnayong <nuannimprasert@gmail.com>
2025-07-16 23:53:20 +0700

    update file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true }, // ถ้าใช้ <Image />
+  basePath: '',
 };
 
-export default withNextIntl(nextConfig) ;
+export default withNextIntl(nextConfig);
